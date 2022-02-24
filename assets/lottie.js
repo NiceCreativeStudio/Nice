c5505c31001f058c89c6dfa23e18517a212b72b2
IgnacioCurto <ignacio.n.curto@gmail.com>
2022-02-23 22:20:14 -0300

    First progress merging

---
+++ assets/lottie.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets5.lottiefiles.com/packages/lf20_rgxh9taz.json' // lottie file path
+  })
